98da7a0e2400fcf06de04fb611b261fb9b13b165
Martijn Lenderink <m.lenderink@gynzy.com>
2024-01-13 15:57:46 +0100

    center items

--- src/components/Page.js
+++ src/components/Page.js
@@ -3,7 +3,7 @@ import * as React from "react";
 const Page = ({ children }) => {
   return (
     <div className="w-full flex-grow flex justify-center">
-      <div className="container flex flex-col">{children}</div>
+      <div className="container flex flex-col items-center">{children}</div>
     </div>
   );
 };
