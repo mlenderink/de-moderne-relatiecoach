6236f293e84130770888221e6686c9becbfb6290
Martijn Lenderink <m.lenderink@gynzy.com>
2024-01-14 10:48:34 +0100

    Add captcha

--- src/templates/contact-page.js
+++ src/templates/contact-page.js
@@ -89,6 +89,7 @@ const AanbodPage = ({ data }) => {
               ></textarea>
             </div>
             <div>
+              <div class="h-captcha" data-captcha="true"></div>
               <button className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none">
                 Verzenden
               </button>
